8b532709870e58b44ebcc9932042d93e50fcc39a
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 15:33:02 +0400

    style(service-zone): normalize entity annotation order

--- src/main/java/com/example/LogisticsNetworkSystem/entities/ServiceZone.java
+++ src/main/java/com/example/LogisticsNetworkSystem/entities/ServiceZone.java
@@ -9,9 +9,9 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Getter
 @Setter
+@Entity
 public class ServiceZone extends BaseClass {
     private String name;
     private String region;
