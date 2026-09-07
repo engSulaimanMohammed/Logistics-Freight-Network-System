d0c5fc5519a4dd8964799aaed17e7d401731a044
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 12:39:54 +0400

    docs(inventory-item): document controller responsibility

--- src/main/java/com/example/LogisticsNetworkSystem/controllers/InventoryItemController.java
+++ src/main/java/com/example/LogisticsNetworkSystem/controllers/InventoryItemController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
+/** Exposes REST operations for warehouse inventory item management. */
 @RestController
 @RequestMapping("/inventoryItem")
 public class InventoryItemController {
