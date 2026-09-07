2dee34b4972a736db494c9e540391ccd5a47a598
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:28:48 +0400

    refactor(vehicle-service): remove redundant service import

--- src/main/java/com/example/LogisticsNetworkSystem/services/VehicleService.java
+++ src/main/java/com/example/LogisticsNetworkSystem/services/VehicleService.java
@@ -4,7 +4,6 @@ import com.example.LogisticsNetworkSystem.entities.Carrier;
 import com.example.LogisticsNetworkSystem.entities.Vehicle;
 import com.example.LogisticsNetworkSystem.exceptions.ResourceNotFoundException;
 import com.example.LogisticsNetworkSystem.repositories.VehicleRepository;
-import com.example.LogisticsNetworkSystem.services.CarrierService;
 import java.util.Date;
 import java.util.List;
 import org.springframework.stereotype.Service;
