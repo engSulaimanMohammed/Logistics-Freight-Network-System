5cc7503cb4105c6e3d0c24db4e6a3a2deaf89f4c
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 15:31:37 +0400

    style(route): normalize entity annotation order

--- src/main/java/com/example/LogisticsNetworkSystem/entities/Route.java
+++ src/main/java/com/example/LogisticsNetworkSystem/entities/Route.java
@@ -10,9 +10,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Entity
 @Getter
 @Setter
+@Entity
 public class Route extends BaseClass {
     private Date routeDate;
     private String origin;
