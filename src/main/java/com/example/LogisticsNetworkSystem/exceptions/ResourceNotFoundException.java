0d36739bcd5c64c78dc74bf4616bd675d92df88c
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 17:10:27 +0400

    refactor(resource-not-found): support exception causes

--- src/main/java/com/example/LogisticsNetworkSystem/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/LogisticsNetworkSystem/exceptions/ResourceNotFoundException.java
@@ -9,4 +9,11 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
+    public ResourceNotFoundException(
+            String message,
+            Throwable cause
+    ) {
+        super(message, cause);
+    }
 }
