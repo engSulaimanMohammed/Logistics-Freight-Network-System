fc61e29c809eb842f4b93c39d7f272cb12a95a41
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:59:53 +0400

    refactor(vehicle-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/VehicleRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/VehicleRepository.java
@@ -4,8 +4,10 @@ import com.example.LogisticsNetworkSystem.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     @Query("SELECT v FROM Vehicle v WHERE LOWER(v.status) = LOWER(:status) AND v.isActive = true")
     List<Vehicle> findByStatus(@Param("status") String status);
