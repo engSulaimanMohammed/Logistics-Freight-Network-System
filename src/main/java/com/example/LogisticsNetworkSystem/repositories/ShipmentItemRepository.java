de5d1120716728d050275dd7db1fb43cf07b8be6
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:58:23 +0400

    refactor(shipment-item-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/ShipmentItemRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/ShipmentItemRepository.java
@@ -2,6 +2,8 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.ShipmentItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShipmentItemRepository extends JpaRepository<ShipmentItem, Long> {
 }
