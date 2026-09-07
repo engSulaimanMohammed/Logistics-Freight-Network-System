2b00d61bd0662f45b180c4f24356f86235acf00e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:59:35 +0400

    refactor(tracking-event-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/TrackingEventRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/TrackingEventRepository.java
@@ -2,6 +2,8 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.TrackingEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TrackingEventRepository extends JpaRepository<TrackingEvent, Long> {
 }
