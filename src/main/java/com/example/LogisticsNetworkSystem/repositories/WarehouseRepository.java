dff5f609f47e2e9e5f3da3956405a24440167474
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 17:00:04 +0400

    refactor(warehouse-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/WarehouseRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/WarehouseRepository.java
@@ -2,6 +2,8 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
 }
