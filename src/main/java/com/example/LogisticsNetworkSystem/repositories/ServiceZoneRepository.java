50b0ae9be88db0d04a5b69720292bde51d00f4cb
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:58:11 +0400

    refactor(service-zone-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/ServiceZoneRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/ServiceZoneRepository.java
@@ -2,6 +2,8 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.ServiceZone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceZoneRepository extends JpaRepository<ServiceZone, Long> {
 }
