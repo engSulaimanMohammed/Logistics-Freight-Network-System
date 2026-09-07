3d320acf56ffe4a7c485b92127a566f1643c4487
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 15:38:02 +0400

    style(warehouse): normalize entity annotation order

--- src/main/java/com/example/LogisticsNetworkSystem/entities/Warehouse.java
+++ src/main/java/com/example/LogisticsNetworkSystem/entities/Warehouse.java
@@ -8,9 +8,9 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Getter
 @Setter
+@Entity
 public class Warehouse extends BaseClass {
     private String name;
     private String location;
