dcc467dfee0d2c36e670d1d699ef3809f4fab181
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:56:07 +0400

    refactor(driver-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/DriverRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/DriverRepository.java
@@ -2,7 +2,9 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
     long countByCarrier_IdAndIsActiveTrue(Long carrierId);
 }
