85c978cb0aaec81f3dc45513b320cbb8dc85dc25
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:57:57 +0400

    refactor(route-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/RouteRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/RouteRepository.java
@@ -4,9 +4,11 @@ import com.example.LogisticsNetworkSystem.entities.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
     @Query("SELECT r FROM Route r WHERE r.driver.id = :driverId AND r.routeDate = :routeDate AND r.isActive = true")
     List<Route> findRoutesForDriverOnDate(@Param("driverId") Long driverId, @Param("routeDate") Date routeDate);
