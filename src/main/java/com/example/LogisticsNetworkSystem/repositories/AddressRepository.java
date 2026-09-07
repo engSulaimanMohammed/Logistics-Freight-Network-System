911f19f2d9944911e51573a4bd3e4f4dee758209
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:55:09 +0400

    refactor(address-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/AddressRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/AddressRepository.java
@@ -2,6 +2,8 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
