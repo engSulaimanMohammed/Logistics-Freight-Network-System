648a6cebe711d32eadf1632a2950ce9bb4425ad1
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 14:45:00 +0400

    docs(carrier-dto): document dto responsibility

--- src/main/java/com/example/LogisticsNetworkSystem/dtos/CarrierDTO.java
+++ src/main/java/com/example/LogisticsNetworkSystem/dtos/CarrierDTO.java
@@ -11,6 +11,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/** Carries logistics carrier data between the API layer and application services. */
 @Data
 @NoArgsConstructor
 public class CarrierDTO {
