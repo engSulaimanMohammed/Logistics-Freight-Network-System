690e3fd4236bde283511672411f196afb472d82a
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 12:35:53 +0400

    docs(delivery-stop): document controller responsibility

--- src/main/java/com/example/LogisticsNetworkSystem/controllers/DeliveryStopController.java
+++ src/main/java/com/example/LogisticsNetworkSystem/controllers/DeliveryStopController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/** Exposes REST operations for route delivery stop management. */
 @RestController
 @RequestMapping("/deliveryStop")
 public class DeliveryStopController {
