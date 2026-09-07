1c122db81bb7bfe317427c9e1c813b4d7722c344
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 16:59:23 +0400

    refactor(staff-repository): declare repository role

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/StaffRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/StaffRepository.java
@@ -2,6 +2,8 @@ package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StaffRepository extends JpaRepository<Staff, Long> {
 }
