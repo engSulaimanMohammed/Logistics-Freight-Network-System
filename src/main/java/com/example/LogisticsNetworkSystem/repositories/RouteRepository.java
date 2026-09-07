730c087333ad90428be8c2c445005770d8ce4358
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 17:01:23 +0400

    style(route-repository): normalize import ordering

--- src/main/java/com/example/LogisticsNetworkSystem/repositories/RouteRepository.java
+++ src/main/java/com/example/LogisticsNetworkSystem/repositories/RouteRepository.java
@@ -1,12 +1,12 @@
 package com.example.LogisticsNetworkSystem.repositories;
 
 import com.example.LogisticsNetworkSystem.entities.Route;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
