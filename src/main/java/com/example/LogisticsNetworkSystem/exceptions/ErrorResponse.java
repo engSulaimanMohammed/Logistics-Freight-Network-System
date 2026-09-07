433852361d51d76c2e07f8b8fa0e0517b074a30a
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 17:09:14 +0400

    style(error-response): format response component layout

--- src/main/java/com/example/LogisticsNetworkSystem/exceptions/ErrorResponse.java
+++ src/main/java/com/example/LogisticsNetworkSystem/exceptions/ErrorResponse.java
@@ -2,5 +2,10 @@ package com.example.LogisticsNetworkSystem.exceptions;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponse(int status, String error, String message, LocalDateTime timestamp) {
+public record ErrorResponse(
+        int status,
+        String error,
+        String message,
+        LocalDateTime timestamp
+) {
 }
