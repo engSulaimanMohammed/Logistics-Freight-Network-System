d293faa5c88ec1d7f37600bedfee8d3c1014e5a1
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 15:32:00 +0400

    style(route): organize route imports

--- src/main/java/com/example/LogisticsNetworkSystem/entities/Route.java
+++ src/main/java/com/example/LogisticsNetworkSystem/entities/Route.java
@@ -1,14 +1,15 @@
 package com.example.LogisticsNetworkSystem.entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Getter
 @Setter
