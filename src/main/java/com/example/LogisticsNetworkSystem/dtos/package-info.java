0fecabfd9e2ade4c0e9c44b8523c1adcab8feada
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-07 14:43:01 +0400

    docs(dtos): add dto package documentation

---
+++ src/main/java/com/example/LogisticsNetworkSystem/dtos/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains Data Transfer Objects used to transfer structured data between
+ * the API/controller layer and other application layers.
+ */
+package com.example.LogisticsNetworkSystem.dtos;
